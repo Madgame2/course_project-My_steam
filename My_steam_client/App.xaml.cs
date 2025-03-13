1cf7015a8af0b07eb8a93845ed2b63911b4dd902
Anton <astakheyko@bk.ru>
2025-03-14 01:56:18 +0300

    initial commit

---
+++ My_steam_client/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace My_steam_client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+
+}
