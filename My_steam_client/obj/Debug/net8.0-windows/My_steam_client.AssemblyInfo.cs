d23491ddf7a0c6283656fe27c698a6992b1b59e5
Anton <astakheyko@bk.ru>
2025-04-15 00:08:38 +0300

    style changes

--- My_steam_client/obj/Debug/net8.0-windows/My_steam_client.AssemblyInfo.cs
+++ My_steam_client/obj/Debug/net8.0-windows/My_steam_client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My_steam_client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838106a833f4d946dfd7b4bf34e05a6136f19288")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b428cb80e867bffe58461cdfdcf62a402b4a8fa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("My_steam_client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My_steam_client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
