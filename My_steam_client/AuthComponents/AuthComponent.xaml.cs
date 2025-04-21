3b5b22e8fc282299b2f26db69e48c404576d62b1
Anton <astakheyko@bk.ru>
2025-04-21 11:25:21 +0300

    added some scripts to auth Window

--- My_steam_client/AuthComponents/AuthComponent.xaml.cs
+++ My_steam_client/AuthComponents/AuthComponent.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,53 @@ namespace My_steam_client.AuthComponents
     /// </summary>
     public partial class AuthComponent : UserControl
     {
+        bool _isValidEmail = false;
+        bool _isValidPassword = false;
+
         public AuthComponent()
         {
             InitializeComponent();
+        }
+
+        private void CancleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+        private bool isValidEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                return false;
+            }
+
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, pattern);
+        }
+
+        private void email_textChanged(object sender, TextChangedEventArgs e)
+        {
+            string curentText = ((TextBox)sender).Text;
+
+            _isValidEmail = isValidEmail(curentText);
+
+            isValidData();
+        }
+
+        private void password_textChanged(object sender, RoutedEventArgs e)
+        {
+            string password = ((PasswordBox)sender).Password;
+
+            if(string.IsNullOrEmpty(password)) _isValidPassword = false;
+            else _isValidPassword = true;
+
+            isValidData();
+        }
+
+        private void isValidData()
+        {
+            LogIn_button.IsEnabled = _isValidPassword && _isValidEmail;
         }
     }
 }
