96be57df0f2037a5d44f307419f80274c7217515
Anton <astakheyko@bk.ru>
2025-05-12 09:24:56 +0300

    added new links for CarItems

---
+++ My_steam_server/Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace My_steam_server.Models
+{
+    public class CartItem
+    {
+        public long CartItemId { get; set; }
+        public long UserId { get; set; }
+        public User User { get; set; }
+
+
+        public long PurchaseOptionId { get; set; }
+        public PurchaseOption PurchaseOption { get; set; }
+    }
+}
--- My_steam_server/Models/User.cs
+++ My_steam_server/Models/User.cs
@@ -5,5 +5,7 @@ namespace My_steam_server.Models
     public class User : IdentityUser
     {
         public string Nickname { get; set; }
+
+        public List<CartItem> CartItems { get; set; } = new();
     }
 }
